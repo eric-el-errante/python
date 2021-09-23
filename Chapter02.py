a545923f5de25f5a9247e8c4fbafef2a420d62bd
Eric Meyr <eric.meyr@gmail.com>
2021-09-23 13:52:12 -0600

    updated chapter 02 assignments

---
+++ Chapter02.py
@@ -0,0 +1,20 @@
+# Variable Name Rules
+# must start with letter or underscore_, can have numbers too
+# Case sensitive
+
+""""
++   Addition
+-   Subtraction
+*   Multiplication
+/   Division
+**  Power
+%   Remainder (modulo)
+"""
+
+# Integer Division will results in a floating point result
+
+# User Input
+nam=input("Who are you?")
+# the input() function pauses and waits for user input, and will return a string
+print('Welcome',nam)
+# inam=int(nam) will turn the string value of nam into an integer if a number is the expected input
