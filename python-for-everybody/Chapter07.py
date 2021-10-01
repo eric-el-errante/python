8e47ec8359398d09f8466f55c5cdf14c19e51178
Eric Meyr <eric.meyr@gmail.com>
2021-10-01 10:08:22 -0600

    mkaing sure updated

--- python-for-everybody/Chapter07.py
+++ python-for-everybody/Chapter07.py
@@ -4,3 +4,13 @@
 
 handle = open("words.txt", "r")
 print(handle)
+
+# this will print each line in the file
+for cheese in handle :
+    print(cheese)
+
+# this will print out the number of lines in a file
+count = 0
+for cheese in handle :
+    count = count + 1
+print('Line Count:', count)
